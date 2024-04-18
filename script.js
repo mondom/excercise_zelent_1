4c7e836f0085896090fc743fb60a6c2ac048d3eb
Monika <codingbymon@gmail.com>
2024-04-18 16:01:07 +0200

    finish third task

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// first task↓
 // const input = document.querySelector('.input')
 // const btn = document.querySelector('.button')
 // const text = document.querySelector('.text')
@@ -17,25 +18,69 @@
 
 // btn.addEventListener('click', check)
 
-const input1 = document.querySelector('.input1')
-const input2 = document.querySelector('.input2')
-const btn = document.querySelector('.button')
-const text = document.querySelector('.text')
+// second task↓
+// const input1 = document.querySelector('.input1')
+// const input2 = document.querySelector('.input2')
+// const btn = document.querySelector('.button')
+// const text = document.querySelector('.text')
 
-let arr = []
-// let ourNumbers = ''
+// let arr = []
+// // let ourNumbers = ''
 
-const showNumbers = () => {
-	if (input1.value < input2.value) {
-		for (let i = input1.value; i <= input2.value; i++) {
-			arr.push(i)
-            text.textContent = arr.join(' ')
-            // ourNumbers = ourNumbers + i + ' '
-            // text.textContent = ourNumbers
-            // console.log(ourNumbers);
-            // ↑ 'pushowanie' każdej iteracji do stringa
-		}
+// const showNumbers = () => {
+// 	if (input1.value < input2.value) {
+// 		for (let i = input1.value; i <= input2.value; i++) {
+// 			arr.push(i)
+//             text.textContent = arr.join(' ')
+//             // ourNumbers = ourNumbers + i + ' '
+//             // text.textContent = ourNumbers
+//             // console.log(ourNumbers);
+//             // ↑ 'pushowanie' każdej iteracji do stringa
+// 		}
+// 	} else {
+//         text.textContent = 'pierwsza cyfra musi być mniejsza od drugiej'
+//     }
+// }
+
+// btn.addEventListener('click', showNumbers)
+
+// third task↓
+
+const slider = document.querySelector('.slider')
+
+let number = Math.floor(Math.random() * 5) + 1
+let interval
+
+const changeSlide = () => {
+	slider.setAttribute('src', `slajd${number}.png`)
+	if (number < 5) {
+		number++
+	} else {
+		number = 1
 	}
+	console.log(slider)
 }
 
-btn.addEventListener('click', showNumbers)
+const startInt = () => {
+	interval = setInterval(changeSlide, 4000)
+}
+
+const clearInt = () => {
+	clearInterval(interval)
+}
+
+const clickers = document.querySelectorAll('span')
+
+function changeSlideOnClick() {
+	clearInt()
+	const num = this.textContent.slice(2, -2)
+	// console.log(num)
+	slider.setAttribute('src', `slajd${num}.png`)
+	startInt()
+}
+
+clickers.forEach(clicker => {
+	clicker.addEventListener('click', changeSlideOnClick)
+})
+
+document.addEventListener('DOMContentLoaded', startInt)
