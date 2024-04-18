43f7438633e5b58cbcd2887cc695200b68e4369e
Monika <codingbymon@gmail.com>
2024-04-18 14:03:36 +0200

    finish second task

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-const input = document.querySelector('.input')
+// const input = document.querySelector('.input')
+// const btn = document.querySelector('.button')
+// const text = document.querySelector('.text')
+
+// const check = () => {
+// 	if (input.value > 0) {
+// 		text.textContent = 'dodatnia'
+// 	} else if (input.value === '0') {
+// 		text.textContent = 'zero'
+// 	} else if (input.value < 0) {
+// 		text.textContent = 'ujemna'
+// 	} else {
+//         text.textContent = 'podaj jakąś liczbę'
+//     }
+
+// }
+
+// btn.addEventListener('click', check)
+
+const input1 = document.querySelector('.input1')
+const input2 = document.querySelector('.input2')
 const btn = document.querySelector('.button')
 const text = document.querySelector('.text')
 
-const check = () => {
-	if (input.value > 0) {
-		text.textContent = 'dodatnia'
-	} else if (input.value === '0') {
-		text.textContent = 'zero'
-	} else if (input.value < 0) {
-		text.textContent = 'ujemna'
-	} else {
-        text.textContent = 'podaj jakąś liczbę'
-    }
-    
+let arr = []
+
+const showNumbers = () => {
+	if (input1.value < input2.value) {
+		for (let i = input1.value; i <= input2.value; i++) {
+			arr.push(i)
+            text.textContent = arr.join(' ')
+		}
+	}
 }
 
-btn.addEventListener('click', check)
+btn.addEventListener('click', showNumbers)
